e0b5ece3aad8b19e581aeda665df7515fe335cfc
minaj <minajinamdar9@gmail.com>
2023-09-16 20:36:54 +0530

    added score display and restart button

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ var rdn;
 function scoreincrement(){
     score += 10;
     document.querySelector("#score").textContent = score;
-}
+};
 
 function bubblemaker(){
-cluster = "";
-for (let i = 1; i < 153; i++){
+ cluster = "";
+ for (let i = 1; i < 153; i++){
     let num = Math.floor(Math.random()*10);
     cluster += `<div id="bubbles" class="bubble color-white cursor-pointer">${num}</div>`;
-}
+ }
 
-document.querySelector(".gamebody").innerHTML = cluster;
-}
-
+ document.querySelector(".gamebody").innerHTML = cluster;
+};
 
 function timerchange(){
     let setInter = setInterval(()=>{
@@ -26,16 +25,18 @@ function timerchange(){
         }
         else{
             clearInterval(setInter);
-            document.querySelector(".gamebody").innerHTML = "<h2>Game Over</h2>";
+            document.querySelector(".gamebody").innerHTML = 
+            `<div class="flex flex-column justify-center align-center"> <h1>Game Over</h1> 
+             <h1>score: ${score}</h1> 
+             <button class = "btn f-bold cursor-pointer" onclick="window.location.reload();">Start Over</button><div>`;
         }
-    },1000)
-    
-}
+    },1000); 
+};
 
 function hitchange(){
     rdn = Math.floor(Math.random()*10);
     document.querySelector("#hit").textContent = rdn;
-}
+};
 
 function hitmatch(){
     document.querySelector(".gamebody").addEventListener("click",(element)=>{
@@ -45,8 +46,14 @@ function hitmatch(){
             bubblemaker();
             hitchange();
         }
+        else{
+            element.target.classList.add("color-red");
+            setTimeout(()=>{
+                element.target.classList.remove("color-red");
+            },200)
+        }
     })
-}
+};
 
 
 bubblemaker();
